d6bede6b957d5ffb77d70df7a929753e64eb27fc
Nyarkits51 <111108641+nyarkits51@users.noreply.github.com>
2024-06-16 00:35:08 +0900

    Clone E869120/kyopro-tessoku
    Clone E869120/kyopro-tessoku
    
    Hi, I think that "kyopro-tessoku" is the very concise and elegant algorithm book for the beginners and advanced programmers to learn how efficient codes to write in. But it's found several faults in the codes of the repos. So, I want to verify the codes to know what is the proper answers in author's algorithm solutions and its real and true meanings.

--- editorial/chap09/cpp/answer_B63.cpp
+++ editorial/chap09/cpp/answer_B63.cpp
@@ -16,8 +16,8 @@ vector<int> G[2509];
 int main() {
 	// 入力
 	cin >> H >> W;
-	cin >> sx >> sy; start = sx * W + sy;
-	cin >> gx >> gy; goal = gx * W + gy;
+	cin >> sx >> sy; start = (sx-1) * W + sy;
+	cin >> gx >> gy; goal = (gx-1) * W + gy;
 	for (int i = 1; i <= H; i++) {
 		for (int j = 1; j <= W; j++) cin >> c[i][j];
 	}
@@ -25,8 +25,8 @@ int main() {
 	// 横方向の辺 [(i, j) - (i, j+1)] をグラフに追加
 	for (int i = 1; i <= H; i++) {
 		for (int j = 1; j <= W - 1; j++) {
-			int idx1 = i * W + j; // 頂点 (i, j) の頂点番号
-			int idx2 = i * W + (j + 1); // 頂点 (i, j+1) の頂点番号
+			int idx1 = (i-1) * W + j; // 頂点 (i, j) の頂点番号
+			int idx2 = (i-1) * W + (j + 1); // 頂点 (i, j+1) の頂点番号
 			if (c[i][j] == '.' && c[i][j + 1] == '.') {
 				G[idx1].push_back(idx2);
 				G[idx2].push_back(idx1);
@@ -37,8 +37,8 @@ int main() {
 	// 縦方向の辺 [(i, j) - (i+1, j)] をグラフに追加
 	for (int i = 1; i <= H - 1; i++) {
 		for (int j = 1; j <= W; j++) {
-			int idx1 = i * W + j; // 頂点 (i, j) の頂点番号
-			int idx2 = (i + 1) * W + j; // 頂点 (i+1, j) の頂点番号
+			int idx1 = (i-1) * W + j; // 頂点 (i, j) の頂点番号
+			int idx2 = i * W + j; // 頂点 (i+1, j) の頂点番号
 			if (c[i][j] == '.' && c[i + 1][j] == '.') {
 				G[idx1].push_back(idx2);
 				G[idx2].push_back(idx1);
@@ -55,7 +55,7 @@ int main() {
 	while (!Q.empty()) {
 		int pos = Q.front();
 		Q.pop();
-		for (int i = 0; i < G[pos].size(); i++) {
+		for (int i = 0; i < int(G[pos].size()); i++) {
 			int to = G[pos][i];
 			if (dist[to] == -1) {
 				dist[to] = dist[pos] + 1;
